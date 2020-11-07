423e823437da5726943cd44d6b2ec554fcd56da8
Dinuka Wanasinghe <Dinuka_105219@dialog.dialoggsm.com>
2020-11-07 23:20:38 +0530

    implement music upload and retrieve functionality to skydb

---
+++ src/sky.js
@@ -0,0 +1,28 @@
+import { SkynetClient, keyPairFromSeed } from "skynet-js";
+
+const client = new SkynetClient();
+const { publicKey, privateKey } = keyPairFromSeed("SY@m78HT9+ZFAfTbQY/Z}}2FNa;v&b]J");
+const dataKey = "skymusic.json";
+export async function uploadExample(file) {
+	try {
+	  return  client.uploadFile(file)
+	} catch (error) {
+	  console.log(error)
+	}
+  }
+
+  export async function updateSkyMusicDb(json) {
+  try {
+    await client.db.setJSON(privateKey, dataKey, json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getSkyMusicDb() {
+	try {
+		return  client.db.getJSON(publicKey, dataKey);
+	} catch (error) {
+	  console.log(error);
+	}
+  }
